719e81cf68426b9d1987b590edfbd78cd88e5577
Kristian WS <kristianws@gmail.com>
2025-11-21 14:42:02 +0700

    Cukup

--- model/User.java
+++ model/User.java
@@ -1,40 +1,18 @@
 package model;
 
 public class User {
-    private int id;
     private String name;
     private String pass;
     private String phone;
-    private String role;
 
-    public User(int id, String pass, String name, String phone, String role) {
-        assert id >= 0;
+    public User(String name, String pass, String phone) {
         assert pass != null && !pass.isBlank();
         assert name != null && !name.isBlank();
         assert phone != null && phone.length() >= 7;
-        assert role == "ADMIN";
 
-        this.id = id;
         this.pass = pass;
         this.name = name;
         this.phone = phone;
-        this.role = role;
-    }
-
-    public User(int id, String nama, String password, String phone) {
-        assert password != null && !pass.isBlank();
-        assert nama != null && !name.isBlank();
-        assert phone != null && phone.length() >= 7;
-
-        this.id = id;
-        this.name = nama;
-        this.pass = password;
-        this.phone = phone;
-        this.role = "USER";
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getPass() {
@@ -49,9 +27,6 @@ public class User {
         return phone;
     }
 
-    public String getRole() {
-        return role;
-    }
 
     public void setName(String name) {
         this.name = name;
@@ -63,12 +38,12 @@ public class User {
 
     @Override
     public String toString() {
-        return id + " | " + name + " | " + phone;
+        return name + " (" + phone + ")";
     }
 
     // file: id|name|phone
     public String toFileString() {
-        return id + "|" + name + "|" + phone + "|" + pass + "|" + role;
+        return name + "|" + phone + "|" + pass ;
     }
 
     public static User fromFileToString(String line) throws Exception {
@@ -78,7 +53,7 @@ public class User {
             throw new Exception("Format File User Salah : " + line);
         }
 
-        User baru = new User(Integer.parseInt(data[0]),data[1], data[2], data[3], data[4]);
+        User baru = new User(data[1], data[2], data[3]);
         return baru;
     }
 
